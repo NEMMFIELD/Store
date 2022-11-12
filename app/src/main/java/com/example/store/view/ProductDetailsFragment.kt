a82d7b3aaf46b259832c9ec458f55ebe6e7efbff
Nikita <markov-nik@yandex.ru>
2022-11-12 23:42:31 +0300

    editing

--- app/src/main/java/com/example/store/view/ProductDetailsFragment.kt
+++ app/src/main/java/com/example/store/view/ProductDetailsFragment.kt
@@ -1,16 +1,26 @@
 package com.example.store.view
 
 import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.TextPaint
+import android.text.style.CharacterStyle
+import android.text.style.ForegroundColorSpan
+import android.text.style.UnderlineSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.store.R
 import com.example.store.databinding.FragmentProductDetailsBinding
 import com.example.store.model.details.CarouselDetailsAdapter
 import com.example.store.viewmodel.HomeStoreViewModel
@@ -47,11 +57,30 @@ class ProductDetailsFragment : Fragment() {
                     }
                 }
             }
+            val text = "Shop"
+            val mSpannableString = SpannableString(text)
+            mSpannableString.setSpan(ColoredUnderlineSpan(Color.parseColor("#FF6E4E"), 10f),
+                0,
+                mSpannableString.length,
+                SpannableString.SPAN_INCLUSIVE_EXCLUSIVE)
+            detailsShop.text = mSpannableString
         }
     }
 
     companion object {
         @JvmStatic
         fun newInstance() = ProductDetailsFragment()
+    }
+
+}
+
+class ColoredUnderlineSpan constructor(
+    private val underlineColor: Int,
+    private val underlineThickness: Float,
+) : CharacterStyle() {
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun updateDrawState(textPaint: TextPaint) {
+        textPaint.underlineColor = underlineColor
+        textPaint.underlineThickness = underlineThickness
     }
 }
